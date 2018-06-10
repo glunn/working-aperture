dbc4b67a657818dfa347b27dcfb6163dd7b5ecd1
Abbie Glunn <aglunn@gmail.com>
2018-06-10 14:52:00 -0700

    Add basic image grid to recent works

--- app/routes/recent-works.js
+++ app/routes/recent-works.js
@@ -6,12 +6,29 @@ export default Route.extend({
       id: 'drill-bits',
       title: 'Drill Bits',
       company: 'Milner',
-      category: 'products'
-    }, {
+      category: 'products',
+      photo: '/img/bottle-on-white.jpg'
+    }, 
+    {
       id: 'sander',
       title: 'Sander',
       company: 'Milner',
-      category: 'products' 
+      category: 'products',
+      photo: '/img/bottle-background.jpg'
+    },
+    {
+      id: 'widget',
+      title: 'Widget',
+      company: 'Coner',
+      category: 'products',
+      photo: '/img/bottle-background.jpg'
+    },
+    {
+      id: 'screw',
+      title: 'Screw',
+      company: 'Taned',
+      category: 'products',
+      photo: '/img/bottle-background.jpg'
     }]
   }
 });
