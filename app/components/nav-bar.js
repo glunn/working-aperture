4814804b940e59950af1c3d2baf73c0e5fe977ed
Abbie Glunn <aglunn@gmail.com>
2018-10-02 00:51:32 -0700

    Partial update of navbar click behavior

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
 
-export default Component.extend({
+export default Component.extend ({
+  actions: {
+    closeNavbar: function() {
+    //  this.toggleProperty('isShowingNavbar');
+      $('.collapse').collapse('hide');
+      // alert('DoubleClickableComponent was clicked!');
+    }
+  }
+  //   // focusOut() {
+  //   // alert('DoubleClickableComponent was clicked!');
+  // }
 });
